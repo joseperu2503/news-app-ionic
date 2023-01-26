6a6bc7d5af6b23eb3afebd6f57e8b6add91fc989
Joseperez <joseperez@wydnex.com>
2023-01-25 21:42:20 -0500

    compartir

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from 'src/app/interfaces';
+import { Browser } from '@capacitor/browser';
+import { ActionSheetController } from '@ionic/angular';
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-article',
@@ -9,8 +12,46 @@ import { Article } from 'src/app/interfaces';
 export class ArticleComponent implements OnInit {
   @Input() article: Article
   @Input() index: number
-  constructor() { }
+  constructor(
+    private actionSheetCtrl: ActionSheetController
+  ) { }
 
   ngOnInit() {}
 
+  async onOpenMenu(){
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Opciones',
+      buttons: [
+        {
+          text: 'Compartir',
+          icon: 'share-outline',
+          handler: () => this.onShareArticle()
+        },
+        {
+          text: 'Favorito',
+          icon: 'heart-outline',
+          handler: () => this.onToggleFavorite()
+        }
+      ]
+    })
+
+    await actionSheet.present()
+  }
+
+  async openArticle(){
+    await Browser.open({ url: this.article.url });
+  }
+
+  async onShareArticle(){
+    await Share.share({
+      title: 'See cool stuff',
+      text: this.article.title,
+      url: this.article.url,
+      dialogTitle: 'Comparte con amigos!'
+    });
+  }
+
+  onToggleFavorite(){
+    console.log('onToggleFavorite')
+  }
 }
